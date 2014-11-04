5f28d1eacdbc225a2bb81a551bb9683e797a77bc
Corey Cossentino <corey@omniti.com>
2014-11-04 17:25:57 -0500

    fix required packages in setup.py

--- app/setup.py
+++ app/setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="KegMeter",
+    name="kegmeter",
     version="0.1",
     author="OmniTI Computer Consulting, Inc.",
     author_email="hello@omniti.com",
     license="MIT",
     packages=find_packages(),
     install_requires=[
-        "memcache >= 1.53",
-        "tornado >= 4.0",
         "oauth2client >= 1.3.1",
+        "pygobject >= 3.8.2",
+        "pyserial >= 2.7",
+        "pysqlite >= 2.6.3",
+        "python-memcached >= 1.53",
+        "requests >= 1.2.3",
+        "simplejson >= 3.6.5",
+        "tornado >= 4.0.2",
         ],
     )
